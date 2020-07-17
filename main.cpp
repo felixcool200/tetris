4e11527b8ad70fa8cf47f57b51785d2135c9ca0c
BuildTools <unconfigured@null.spigotmc.org>
2020-07-17 21:17:04 +0000

    Rerote base from scratch

--- main.cpp
+++ main.cpp
@@ -1,52 +1,62 @@
 #include <iostream>
 #include <chrono>
-#include <string>
+#include <thread>
 
-using namespace std;
+using namespace std::literals::chrono_literals;
 
-bool running = false;
+static bool running = false;
 int boardSize = 10;
+int posX = 0;
+int posY = 0;
 
-class block{
-public:
-int x = 0;
-int y = 0;
-void MoveBlock(){
-    if(y > boardSize){y++;}
+void update(){
+    	std::cout << "\033[2J\033[1;1H";    
+    	char board[10][10];
+		if(posX >= 10 || posY >= 10){
+			running = false;
+			return;
+		}
+    	for(int i = 0; i < 10;i++){
+           	for(int j = 0; j < 10;j++){
+                	board[i][j] = '@';
+                	board[posX][posY] = 'P';
+                	std::cout << board[i][j];
+            	}  
+       	std::cout << "\n";
+    	} 
 }
-};
 
-void update(block o){ 
-    cout << "\033[2J\033[1;1H";    
-    char board[10][10];
-    for(int i = 0; i < 10;i++){
-           for(int j = 0; j < 10;j++){
-                board[i][j] = '@';
-                board[o.y][o.x] = 'P';
-                cout << board[i][j];
-            }  
-        cout << "\n";
-    } 
+void inputLoop(){
+	char key;
+	while(true){
+		std::cin >> key;
+		switch(key){
+		case 'w':
+			posX--;
+			break;
+		case 'a':
+			posY--;
+			break;}
+	}
 }
+
 
 void run(){
-    running = true;
-    auto previous = chrono::high_resolution_clock::now();
-    block obj1;
-    while(running){
-        auto now = chrono::high_resolution_clock::now();
-        chrono::duration<double> D = now - previous;
-        if(D.count() >= 1){
-            previous = now;
-            obj1.MoveBlock();
-            update(obj1);}
+    running = true; 
+	while(running){
+		std::this_thread::sleep_for(1s);
+		posX += 1;
+		posY += 1;
+		update();
     }
+	return;
 }
+
 
 int main(){
-    cout << "Hello World" << endl;
-    cout << "This is Tetris" << endl;
-    run();
-    return 0;
+    std::cout << "Hello World" << std::endl;
+    std::cout << "This is Tetris" << std::endl;	
+	std::thread gameThread(run);
+	inputLoop();
+	return 0;
 }
-
