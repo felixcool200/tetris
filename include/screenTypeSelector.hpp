743ca22e576ecaaabc31c1e351cb324430b8fcdd
Felix Söderman <felixsoderman+github@gmail.com>
2025-05-17 20:45:41 +0200

    Rename ScreenToUse and start to remove the Game/Tetromino template classes.

---
+++ include/screenTypeSelector.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+// Define the screen type based on the preprocessor macro passed by CMake
+#ifdef SCREEN_NCURSES
+#include <ncursesScreen.hpp>
+using ScreenType = Screen::NcursesScreen;
+
+#elif defined(SCREEN_RAYLIB)
+#include <raylibScreen.hpp>
+using ScreenType = Screen::RaylibScreen;
+
+#else
+#error "No valid SCREEN_TYPE defined!"
+#endif
+
+#include <screenInterface.hpp>
+static_assert(Screen::ScreenInterface<ScreenType>, "Selected screen does not implement interface");
